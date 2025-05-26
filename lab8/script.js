5e3157b3a00727a16e8bc9f2ebc0975e6e61977e
Ddiiaanaa <114647803+Ddiiaanaa@users.noreply.github.com>
2025-05-27 02:16:43 +0300

    Update script.js

--- lab8/script.js
+++ lab8/script.js
@@ -4,22 +4,15 @@ const indicators = document.querySelectorAll('.indicator');
 const totalSlides = slides.length;
 let slideInterval;
 
-// Перемикання меню
 function toggleMenu() {
     const menu = document.querySelector('.menu-items');
     menu.classList.toggle('active');
 }
 
-// Карусель
 function moveSlide(direction) {
-    // Зупиняємо автопрокрутку
     clearInterval(slideInterval);
-
-    // Оновлюємо індекс слайду
     currentSlide = (currentSlide + direction + totalSlides) % totalSlides;
     updateCarousel();
-
-    // Поновлюємо автопрокрутку
     startSlideInterval();
 }
 
@@ -31,14 +24,9 @@ function goToSlide(index) {
 }
 
 function updateCarousel() {
-    // Оновлюємо позицію слайдів
     const offset = -currentSlide * 100;
     document.querySelector('.carousel-slides').style.transform = `translateX(${offset}%)`;
 
-    // Оновлюємо активний слайд та індикатор
-    slides.forEach((slide, index) => {
-        slide.classList.toggle('active', index === currentSlide);
-    });
     indicators.forEach((indicator, index) => {
         indicator.classList.toggle('active', index === currentSlide);
     });
@@ -47,9 +35,7 @@ function updateCarousel() {
 function startSlideInterval() {
     slideInterval = setInterval(() => {
         moveSlide(1);
-    }, 3000); // Зміна слайду кожні 3 секунди
+    }, 3000);
 }
-
-// Початковий запуск каруселі
 updateCarousel();
 startSlideInterval();
